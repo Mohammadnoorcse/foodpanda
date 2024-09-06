e6bd427419c704203173b0a770cd4ae86c1fa3bd
mohammadnoor <mohammadnoorcse@gmail.com>
2024-09-06 16:07:48 +0600

    working app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from './page/Home'
 import Navbar from './component/layout/Navbar'
-
+// working project
 const App = () => {
   return (
     <BrowserRouter>
--- client/src/component/layout/TopNavbar.jsx
+++ client/src/component/layout/TopNavbar.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const TopNavbar = () => {
   return (
-    <div>TopNavbar</div>
-  )
+    <div className="w-full h-[5rem] bg-[#e21b70] text-white flex">
+      <div className="w-[98%] h-full flex justify-center items-center gap-4">
+        <img
+          src="https://micro-assets.foodora.com/img/ic-rider-icon.svg"
+          alt=""
+          className="w-[48px] h-[48px] text-white"
+        />
+        <span className="text-xl font-bold">
+          Do you need a business account?
+        </span>
+        <button className="border-2 p-2 rounded-md text-sm font-bold hover:bg-white hover:text-[#A29EA4]">
+          SIGN UP NOW
+        </button>
+      </div>
+      <div className="w-[2%] flex items-center">
+        <span className='cursor-pointer'>x</span>
+      </div>
+    </div>
+  );
 }
 
 export default TopNavbar
