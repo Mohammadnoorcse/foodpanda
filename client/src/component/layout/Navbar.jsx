86ea2b3fdc8a140b9f3397df92ac888379aac11b
mohammadnoor <mohammadnoorcse@gmail.com>
2024-09-07 23:36:56 +0600

    working foodpanda navbar

--- client/src/component/layout/Navbar.jsx
+++ client/src/component/layout/Navbar.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import TopNavbar from './TopNavbar'
+import {Link} from "react-router-dom"
+import logo from "../../assets/panda-logo.png";
 
 const Navbar = () => {
   return (
     <>
-    <TopNavbar/>
-    
-    
+      <TopNavbar />
+      <div className="w-full h-[4rem] border shadow-md flex justify-center items-center">
+        <div className="container flex justify-between items-center">
+          <Link to="/" className="flex gap-2 items-center">
+            <div className="w-7 h-7 bg-[#E21B70] flex justify-center items-center rounded-md">
+              <img src={logo} alt="" className="text-white" />
+            </div>
+            <span className="text-xl font-medium text-[#E21B70]">foodpanda</span>
+          </Link>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Navbar
