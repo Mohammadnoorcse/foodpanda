798329bba4d9fc16dbc3bcaa1cbef9cf7b1ed4ce
mohammadnoor <mohammadnoorcse@gmail.com>
2024-09-08 17:22:15 +0600

    working login or signup page

--- client/src/component/layout/Navbar.jsx
+++ client/src/component/layout/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TopNavbar from './TopNavbar'
 import {Link} from "react-router-dom"
 import logo from "../../assets/panda.png";
+import img1 from "../../assets/login-1.svg";
+import Login from './Login';
+import SignUp from './SignUp';
 
 const Navbar = () => {
+  const[islogin,setLogin] = useState(false);
+  const[isLoginBtn,setIsLoginBtn] = useState("");
+
+  const LoginBtnhandler = (item)=>{
+        setLogin(false);
+        setIsLoginBtn(item);
+  }
+  const LoginBtnArrowhandler = (item)=>{
+        setLogin(true);
+        setIsLoginBtn(item);
+  }
   return (
     <>
       <TopNavbar />
@@ -19,10 +33,16 @@ const Navbar = () => {
             </span>
           </Link>
           <div className="flex gap-3 items-center">
-            <div className="w-[4rem] h-7 flex items-center justify-center border border-[black] rounded-md hover:scale-[1.1] cursor-pointer hover:bg-[#cacaca]">
+            <div
+              className="w-[4rem] h-7 flex items-center justify-center border border-[black] rounded-md hover:scale-[1.1] cursor-pointer hover:bg-[#cacaca]"
+              onClick={() => setLogin(true)}
+            >
               <span className="text-[#39434d] text-sm font-medium">login</span>
             </div>
-            <div className="w-[5rem] h-7 flex items-center justify-center bg-[#E21B70] rounded-md hover:scale-[1.1] cursor-pointer ">
+            <div
+              className="w-[5rem] h-7 flex items-center justify-center bg-[#E21B70] rounded-md hover:scale-[1.1] cursor-pointer "
+              onClick={() => setLogin(true)}
+            >
               <span className="text-[white] text-sm font-medium">Sign up</span>
             </div>
             <div className="w-[5rem] h-7 flex items-center justify-center gap-1 rounded-md hover:bg-[#F0F2F2] cursor-pointer">
@@ -36,9 +56,18 @@ const Navbar = () => {
           </div>
 
           {/* login model */}
-          <div className="absolute top-[20rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-white rounded-lg box-shadow w-[25rem] h-[30rem] z-[500] flex flex-col gap-4 p-6">
+          <div
+            className={
+              islogin
+                ? "absolute top-[20rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-white rounded-lg box-shadow w-[25rem] h-[30rem] z-[500] flex flex-col gap-4 p-6"
+                : "hidden"
+            }
+          >
             <div className="flex justify-end">
-              <div className=" p-2 shadow-md w-[2.1rem] h-[2.1rem] rounded-[100%] flex justify-center items-center border border-dashed cursor-pointer">
+              <div
+                className=" p-2 shadow-md w-[2.1rem] h-[2.1rem] rounded-[100%] flex justify-center items-center border border-dashed cursor-pointer"
+                onClick={() => setLogin(false)}
+              >
                 <i className="fa-solid fa-xmark"></i>
               </div>
             </div>
@@ -63,10 +92,16 @@ const Navbar = () => {
             <div className="w-full text-center">
               <p className="text-[#707070] text-sm font-normal">or</p>
             </div>
-            <div className="w-full h-10 rounded-md bg-[#E11B70] cursor-pointer text-center flex justify-center items-center">
+            <div
+              className="w-full h-10 rounded-md bg-[#E11B70] cursor-pointer text-center flex justify-center items-center"
+              onClick={() => LoginBtnhandler("login")}
+            >
               <span className="text-sm font-bold text-white">Log in</span>
             </div>
-            <div className="w-full h-10 rounded-md border border-black text-[#39434d] cursor-pointer text-center flex justify-center items-center hover:bg-[#fdf2f7] hover:border-[#fdf2f7] hover:text-[#171F26]">
+            <div
+              className="w-full h-10 rounded-md border border-black text-[#39434d] cursor-pointer text-center flex justify-center items-center hover:bg-[#fdf2f7] hover:border-[#fdf2f7] hover:text-[#171F26]"
+              onClick={() => LoginBtnhandler("sign")}
+            >
               <span className="text-sm font-bold  ">Sign up</span>
             </div>
             <p className="text-[#707070] text-sm font-normal text-justify">
@@ -81,6 +116,20 @@ const Navbar = () => {
               </Link>
             </p>
           </div>
+          {/* login from */}
+          {isLoginBtn === "login" ? (
+            <Login
+              LoginBtnArrowhandler={LoginBtnArrowhandler}
+              setIsLoginBtn={setIsLoginBtn}
+            />
+          ) : isLoginBtn === "sign" ? (
+            <SignUp
+              LoginBtnArrowhandler={LoginBtnArrowhandler}
+              setIsLoginBtn={setIsLoginBtn}
+            />
+          ) : (
+            ""
+          )}
         </div>
       </div>
       {/* mobaile Navbar */}
