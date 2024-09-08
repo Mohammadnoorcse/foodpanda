4ff4e80c1340f45453733076d0f6c711cb104ad9
mohammadnoor <mohammadnoorcse@gmail.com>
2024-09-08 10:35:45 +0600

    working navbar for foodpanda

--- client/src/component/layout/Navbar.jsx
+++ client/src/component/layout/Navbar.jsx
@@ -1,20 +1,65 @@
 import React from 'react'
 import TopNavbar from './TopNavbar'
 import {Link} from "react-router-dom"
-import logo from "../../assets/panda-logo.png";
+import logo from "../../assets/panda.png";
 
 const Navbar = () => {
   return (
     <>
       <TopNavbar />
-      <div className="w-full h-[4rem] border shadow-md flex justify-center items-center">
-        <div className="container flex justify-between items-center">
+      {/* desktop Navbar */}
+      <div className="w-full h-[4rem] border shadow-md sm:flex justify-center items-center hidden">
+        <div className="container flex justify-between items-center relative">
           <Link to="/" className="flex gap-2 items-center">
-            <div className="w-7 h-7 bg-[#E21B70] flex justify-center items-center rounded-md">
-              <img src={logo} alt="" className="text-white" />
+            <div className="w-7 h-7 flex justify-center items-center rounded-md">
+              <img src={logo} alt="" />
             </div>
-            <span className="text-xl font-medium text-[#E21B70]">foodpanda</span>
+            <span className="text-xl font-medium text-[#E21B70]">
+              foodpanda
+            </span>
           </Link>
+          <div className="flex gap-3 items-center">
+            <div className="w-[4rem] h-7 flex items-center justify-center border border-[black] rounded-md hover:scale-[1.1] cursor-pointer hover:bg-[#cacaca]">
+              <span className="text-[#39434d] text-sm font-medium">login</span>
+            </div>
+            <div className="w-[5rem] h-7 flex items-center justify-center bg-[#E21B70] rounded-md hover:scale-[1.1] cursor-pointer ">
+              <span className="text-[white] text-sm font-medium">Sign up</span>
+            </div>
+            <div className="w-[5rem] h-7 flex items-center justify-center gap-1 rounded-md hover:bg-[#F0F2F2] cursor-pointer">
+              <i class="fa-solid fa-globe"></i>
+              <span>EN</span>
+              <i class="fa-solid fa-chevron-down text-[#E21B70]"></i>
+            </div>
+            <div className="w-8 h-8 hover:bg-[#F0F2F2] flex justify-center items-center rounded-[100%] cursor-pointer">
+              <i class="fa-solid fa-cart-shopping text-[#4D5358]"></i>
+            </div>
+          </div>
+
+          {/* login model */}
+          <div>
+            
+          </div>
+        </div>
+      </div>
+      {/* mobaile Navbar */}
+      <div className="w-full h-[4rem] border shadow-md sm:hidden flex justify-center items-center ">
+        <div className="container flex justify-between items-center pl-3 pr-3">
+          <div>
+            <i class="fa-solid fa-user text-[#4D5358]"></i>
+          </div>
+
+          <Link to="/" className="flex gap-2 items-center">
+            <div className="w-7 h-7 flex justify-center items-center rounded-md">
+              <img src={logo} alt="" />
+            </div>
+            <span className="text-xl font-medium text-[#E21B70]">
+              foodpanda
+            </span>
+          </Link>
+
+          <div className="w-8 h-8 hover:bg-[#F0F2F2] flex justify-center items-center rounded-[100%] cursor-pointer">
+            <i class="fa-solid fa-cart-shopping text-[#4D5358]"></i>
+          </div>
         </div>
       </div>
     </>
--- client/src/component/layout/TopNavbar.jsx
+++ client/src/component/layout/TopNavbar.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const TopNavbar = () => {
   return (
-    <div className="w-full h-[5rem] bg-[#e21b70] text-white flex">
-      <div className="w-[98%] h-full flex justify-center items-center gap-4">
+    <div className="w-full h-[5rem] bg-[#e21b70] text-white flex ">
+      <div className="sm:w-[98%] w-[95%] h-full flex justify-center items-center gap-4 pl-2">
         <img
           src="https://micro-assets.foodora.com/img/ic-rider-icon.svg"
           alt=""
-          className="w-[48px] h-[48px] text-white"
+          className="w-[48px] h-[48px] text-white sm:flex hidden"
         />
-        <span className="text-xl font-bold">
+        <span className="text-xl sm:font-bold font-normal">
           Do you need a business account?
         </span>
-        <button className="border-2 p-2 rounded-md text-sm font-bold hover:bg-white hover:text-[#A29EA4]">
+        <button className="border-2 sm:p-2 p-1 rounded-md text-sm sm:font-bold font-normal hover:bg-white hover:text-[#A29EA4]">
           SIGN UP NOW
         </button>
       </div>
-      <div className="w-[2%] flex items-center">
+      <div className="sm:w-[2%] w-[5%] pl-1 sm:pl-0 flex items-center">
         <span className='cursor-pointer'>x</span>
       </div>
     </div>
